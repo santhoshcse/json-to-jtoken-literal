6d84c37f6cdcbb57f2c4ee7f40d0e4a32e9e8c6a
Santhosh.Kumar <Santhosh.Kumar@ducenIT.com>
2021-03-22 21:10:21 +0530

    1. Fix: leading `,` for JObject and JArray types. 2. Fix: misleading `,` for some scenarios. 3. Fix: extracted property name from property path. 4. Fix: property name with `"`, other charactors.
    *. Add level property for indentation.

--- JSON2ObjInitializer-BackEnd/JSON2ObjInitializer-BackEnd/JSONSerializerUtil.cs
+++ JSON2ObjInitializer-BackEnd/JSON2ObjInitializer-BackEnd/JSONSerializerUtil.cs
@@ -17,7 +17,7 @@
         public static JToken Deserialize(string jsonString)
         {
             var jsonObject = JsonConvert.DeserializeObject(jsonString);
-            var jObj = JObject.FromObject(jsonObject);
+            var jObj = JToken.FromObject(jsonObject);
             return jObj;
         }
 
@@ -27,7 +27,7 @@
             if (type == 0)
             {
                 Sb = new StringBuilder();
-                ConstructObject(jObj, isLast: true);
+                ConstructObject(jObj, 0, isLast: true);
                 string result = Sb.ToString();
                 result = result.Replace(@"bool~True", "true");
                 result = result.Replace(@"bool~False", "false");
@@ -37,39 +37,42 @@
             return string.Empty;
         }
 
-        private static void ConstructObject(JToken jObj, bool isLast = false)
+        private static void ConstructObject(JToken jObj, int level, bool isLast = false)
         {
             if (jObj.Type == JTokenType.Object)
             {
-                Sb.Append("new JObject(");
+                Sb.Append("new JObject");
+                Sb.Append("\n{\n");
                 foreach (JToken child in jObj.Children())
                 {
                     bool isLastChild = child.Next == null;
-                    ConstructObject(child, isLastChild);
+                    ConstructObject(child, level + 1, isLastChild);
                 }
 
-                Sb.Append(")");
+                Sb.Append("\n}");
                 if (!isLast)
                 {
-                    Sb.Append(", ");
+                    Sb.Append(",\n");
                 }
             }
             else if (jObj.Type == JTokenType.Array)
             {
-                Sb.Append("new JArray(");
+                Sb.Append("new JArray");
+                Sb.Append("\n{\n");
                 foreach (JToken child in jObj.Children())
                 {
-                    ConstructObject(child);
-                    if (child.Next != null)
+                    bool isLastChild = child.Next == null;
+                    ConstructObject(child, level + 1, isLastChild);
+                    if (child.Next != null && (child.Type != JTokenType.Object && child.Type != JTokenType.Array && child.Type != JTokenType.Property))
                     {
                         Sb.Append(", ");
                     }
                 }
 
-                Sb.Append(")");
+                Sb.Append("\n}");
                 if (!isLast)
                 {
-                    Sb.Append(", ");
+                    Sb.Append(",\n");
                 }
             }
             else if (jObj.Type == JTokenType.Property)
@@ -77,15 +80,15 @@
                 Sb.Append("new JProperty(");
                 foreach (JToken child in jObj.Children())
                 {
-                    Sb.Append(string.Format("\"{0}\"", child.Path));
+                    Sb.Append(string.Format("\"{0}\"", GetPropertyName(child.Path)));
                     Sb.Append(", ");
-                    ConstructObject(child, true);
+                    ConstructObject(child, level + 1, true);
                 }
 
                 Sb.Append(")");
                 if (!isLast)
                 {
-                    Sb.Append(", ");
+                    Sb.Append(",\n");
                 }
             }
             else if (jObj.Type == JTokenType.String)
@@ -103,15 +106,50 @@
             }
             else if (jObj.Type == JTokenType.Boolean)
             {
-                // Adding bool~ to the value to have proper bool literal in the output
+                // Adding `bool~` to the value to have proper boolean literal in the output
                 // Ex: To change False/True to false/true
                 Sb.Append("bool~" + jObj.Value<bool>());
             }
             else if (jObj.Type == JTokenType.Null)
             {
-                string NULL_STR = null;
-                Sb.Append(NULL_STR);
+                var nullLiteral = "null";
+                Sb.Append(nullLiteral);
             }
+        }
+
+        private static string GetPropertyName(string propertyPath)
+        {
+            int propertyPathLength = propertyPath.Length;
+            string propertyName;
+            if (propertyPath.EndsWith(']'))
+            {
+                if (propertyPath[propertyPathLength - 2] == '\'')
+                {
+                    // abcde['ab'] => 11-5-4
+                    int propertyKeyStartingIndex = propertyPath.LastIndexOf("['");
+                    propertyName = propertyPath.Substring(propertyKeyStartingIndex + 2, propertyPathLength - propertyKeyStartingIndex - 4);
+                }
+                else
+                {
+                    // abcde["ab"] => 11-5-4
+                    // to be deleted: this case won't occur
+                    int propertyKeyStartingIndex = propertyPath.LastIndexOf("[\"");
+                    propertyName = propertyPath.Substring(propertyKeyStartingIndex + 2, propertyPathLength - propertyKeyStartingIndex - 4);
+                }
+
+                if (propertyName.Contains('"'))
+                {
+                    propertyName = propertyName.Replace("\"", "\\\"");
+                }
+            }
+            else
+            {
+                // abcde.ab => 8-5-1
+                int propertyKeyStartingIndex = propertyPath.LastIndexOf(".");
+                propertyName = propertyPath.Substring(propertyKeyStartingIndex + 1, propertyPathLength - propertyKeyStartingIndex - 1);
+            }
+
+            return propertyName;
         }
     }
 }
--- JSON2ObjInitializer-BackEnd/JSON2ObjInitializer-BackEnd/Program.cs
+++ JSON2ObjInitializer-BackEnd/JSON2ObjInitializer-BackEnd/Program.cs
@@ -6,15 +6,8 @@ namespace JSON2ObjInitializer_BackEnd
     {
         static void Main(string[] args)
         {
-            var jObj = new
-            {
-                Name = "Santhosh",
-                Age = 28,
-                Teams = new [] { "Services", "UI", "Analytics" },
-                Department = "Product",
-                IsIntern = false
-            };
-            string jsonString = JsonSerializerUtil.Serialize(jObj);
+            string jsonString = "{\"data\":[{\"key\":\"Audio\",\"it.e][\\\"ms\":[{\"key\":\"Bluetooth Headphones\",\"items\":null,\"count\":13482,\"summary\":[12099500.9899]}]}],\"totalCount\":1000000,\"summary\":[3638256074.5103]}";
+            string jsonString1 = "[[{\"key\":\"Bluetooth Headphones\",\"items\":null,\"count\":13482,\"summary\":[12099500.9899]}],[\"value\"]]";
             var obj = JsonSerializerUtil.Deserialize(jsonString);
             string objectIniOutput = JsonSerializerUtil.ConstructObjectInitializerFormat(obj);
             Console.WriteLine(objectIniOutput);
